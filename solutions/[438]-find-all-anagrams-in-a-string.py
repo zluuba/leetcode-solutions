ad240eb7b093f372054c32cfa98e9a4631f0cadf
zluuba <zluyba.nikitina@gmail.com>
2023-02-05 10:17:35 +0300

    Feat: add solutions dir, add [438] problem

---
+++ solutions/[438]-find-all-anagrams-in-a-string.py
@@ -0,0 +1,28 @@
+# Given two strings s and p, return an array of all the start indices of p's anagrams in s.
+# You may return the answer in any order.
+#
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+# typically using all the original letters exactly once.
+
+
+class Solution:
+    def get_dict(self, string):
+        d = dict()
+        for char in set(string):
+            count = string.count(char)
+            d[char] = count
+        return d
+
+    def findAnagrams(self, s: str, p: str) -> list[int]:
+        res = []
+        dict_p = self.get_dict(p)
+
+        for ind, char in enumerate(s):
+            if len(s) - ind < len(p):
+                break
+            chunk = s[ind:len(p) + ind]
+            dict_chunk = self.get_dict(chunk)
+
+            if dict_chunk == dict_p:
+                res.append(ind)
+        return res
