0cf13ac7fbdf8453d1b0b4329234b8c725c6b622
zluuba <zluyba.nikitina@gmail.com>
2023-02-07 14:15:42 +0300

    Feat: add [13] problem solution

---
+++ easy-level/[13]-roman-to-integer.py
@@ -0,0 +1,36 @@
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+# For example, 2 is written as II in Roman numeral, just two ones added together.
+# 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+#
+# Roman numerals are usually written largest to smallest from left to right.
+# However, the numeral for four is not IIII. Instead, the number four is written as IV.
+# Because the one is before the five we subtract it making four. The same principle applies to the number nine,
+# which is written as IX. There are six instances where subtraction is used:
+#
+# I can be placed before V (5) and X (10) to make 4 and 9.
+# X can be placed before L (50) and C (100) to make 40 and 90.
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+# Given a roman numeral, convert it to an integer.
+
+# --------------- Runtime 43 ms, beats 90.81%. Memory 13.9MB, beats 24.26% ---------------
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1,
+        }
+
+        int_sum = 0
+        for ind, char in enumerate(s[:-1]):
+            if dic[char] < dic[s[ind + 1]]:
+                int_sum -= dic[char]
+            else:
+                int_sum += dic[char]
+        return int_sum + dic[s[-1]]
