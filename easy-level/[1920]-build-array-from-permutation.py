d2589b27be19635ed641029e900918fa2ebee32b
zluuba <zluyba.nikitina@gmail.com>
2023-02-07 13:00:14 +0300

    Feat: add [1920] problem solution

---
+++ easy-level/[1920]-build-array-from-permutation.py
@@ -0,0 +1,11 @@
+# Given a zero-based permutation nums (0-indexed), build an array ans of the same length
+# where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+#
+# A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+# --------------- Runtime 116 ms, beats 96.20%. Memory 14.1MB, beats 40.70% ---------------
+
+
+class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        return [nums[num] for num in nums]
