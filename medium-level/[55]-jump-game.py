9a03707c39e58b9c525b05005bebcd65863cf99a
zluuba <zluyba.nikitina@gmail.com>
2023-02-08 13:51:24 +0300

    Feat: add [55] problem solution

---
+++ medium-level/[55]-jump-game.py
@@ -0,0 +1,18 @@
+# You are given an integer array nums. You are initially positioned at the array's first index,
+# and each element in the array represents your maximum jump length at that position.
+#
+# Return true if you can reach the last index, or false otherwise.
+
+# --------------- Runtime 519 ms, beats 51.40%. Memory 15.2MB, beats 78.83% ---------------
+
+
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        curr = 0
+
+        for index, number in enumerate(nums):
+            if curr >= index:
+                curr = max(curr, index + number)
+                continue
+            return False
+        return True
