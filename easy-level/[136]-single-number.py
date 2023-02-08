bb1190639f0f586d02734972fa1c3c554710bccc
zluuba <zluyba.nikitina@gmail.com>
2023-02-08 10:34:12 +0300

    Feat: add [136] problem solution

---
+++ easy-level/[136]-single-number.py
@@ -0,0 +1,21 @@
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+#
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+# --------------- Runtime 5956 ms, beats 9.57%. Memory 16.7MB, beats 45.77% ---------------
+
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        for num in nums:
+            if nums.count(num) == 1:
+                return num
+
+
+# Alternative solution. Lowest runtime
+# --------------- Runtime 138 ms, beats 70.3%. Memory 17.1MB, beats 10.12% ---------------
+
+
+class Solution2:
+    def singleNumber(self, nums: list[int]) -> int:
+        return 2 * sum(set(nums)) - sum(nums)
