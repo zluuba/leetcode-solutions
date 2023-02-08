11b6b9e0d59df0c11e3c1be3cf292c0fcefe6b41
zluuba <zluyba.nikitina@gmail.com>
2023-02-08 11:02:27 +0300

    Feat: add [904] solution problem

---
+++ medium-level/[904]-fruit-into-baskets.py
@@ -0,0 +1,31 @@
+# You are visiting a farm that has a single row of fruit trees arranged from left to right.
+# The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+#
+# You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+#
+# You only have two baskets, and each basket can only hold a single type of fruit.
+# There is no limit on the amount of fruit each basket can hold.
+# Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree)
+# while moving to the right. The picked fruits must fit in one of your baskets.
+# Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+# Given the integer array fruits, return the maximum number of fruits you can pick.
+
+# --------------- Runtime 836 ms, beats 93.59%. Memory 20.2MB, beats 51.24% ---------------
+
+
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        basket = {}
+        left = 0
+
+        for right, fruit in enumerate(fruits):
+            basket[fruit] = basket.get(fruit, 0) + 1
+
+            if len(basket) > 2:
+                basket[fruits[left]] -= 1
+
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+
+        return right - left + 1
