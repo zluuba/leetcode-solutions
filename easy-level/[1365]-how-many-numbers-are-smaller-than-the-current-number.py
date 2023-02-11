fa7a38f95ee9b430c0f274f7e2bac878ecafb1f2
zluuba <zluyba.nikitina@gmail.com>
2023-02-11 17:42:25 +0300

    Feat: add [1365], [1678], [1720] problem solutions

---
+++ easy-level/[1365]-how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,15 @@
+# Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+# That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+#
+# Return the answer in an array.
+
+# --------------- Runtime 371 ms, beats 33.20%. Memory 13.8MB, beats 79.67% ---------------
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: list[int]) -> list[int]:
+        result = []
+        for i in nums:
+            count = [1 if i > x else 0 for x in nums if i != x]
+            result.append(sum(count))
+        return result
