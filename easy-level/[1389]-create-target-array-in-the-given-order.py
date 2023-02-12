ca7b38542b016a0338ae17b944039c7acfcc4d77
zluuba <zluyba.nikitina@gmail.com>
2023-02-12 08:35:04 +0300

    Feat: add [1389] problem solution

---
+++ easy-level/[1389]-create-target-array-in-the-given-order.py
@@ -0,0 +1,19 @@
+# Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+#
+# Initially target array is empty.
+# From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+# Repeat the previous step until there are no elements to read in nums and index.
+# Return the target array.
+#
+# It is guaranteed that the insertion operations will be valid.
+
+# --------------- Runtime 33 ms, beats 79.37%. Memory 13.8MB, beats 96.18% ---------------
+
+
+class Solution:
+    def createTargetArray(self, nums: list[int], index: list[int]) -> list[int]:
+        result = []
+        for ind, n in enumerate(index):
+            result.insert(n, nums[ind])
+
+        return result
