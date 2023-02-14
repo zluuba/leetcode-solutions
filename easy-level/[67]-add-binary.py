f67637e091b582e3f66c27690802980b57110043
zluuba <zluyba.nikitina@gmail.com>
2023-02-14 09:23:21 +0300

    Feat: add [67] problem solution

---
+++ easy-level/[67]-add-binary.py
@@ -0,0 +1,41 @@
+# Given two binary strings a and b, return their sum as a binary string.
+
+# Short solution
+# --------------- Runtime 38 ms, beats 53.43%. Memory 13.8MB, beats 63.5% ---------------
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        summ = int(a, 2) + int(b, 2)
+        bin_sum = bin(summ)
+        return bin_sum[2:]
+
+
+# Long solution without bin function
+# --------------- Runtime 35 ms, beats 69.29%. Memory 13.9MB, beats 63.5% ---------------
+
+
+class Solution2:
+    def addBinary(self, a: str, b: str) -> str:
+        zero = '0'
+        one = '1'
+
+        if len(a) > len(b):
+            b = zero * (len(a) - len(b)) + b
+        else:
+            a = zero * (len(b) - len(a)) + a
+
+        result = ''
+        rem = 0
+        for i in range(len(a) - 1, -1, -1):
+            if a[i] == b[i]:
+                result += zero if rem == 0 else one
+                rem = 0 if a[i] == zero else 1
+
+            else:
+                result += one if rem == 0 else zero
+
+            if i == 0 and rem == 1:
+                result += one
+
+        return result[::-1]
