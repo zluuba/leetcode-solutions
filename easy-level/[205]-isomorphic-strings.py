2a02c5713eaf448acd769cf31841eb99315a091d
zluuba <zluyba.nikitina@gmail.com>
2023-02-15 09:05:53 +0300

    Feat: add [205] problem solution

---
+++ easy-level/[205]-isomorphic-strings.py
@@ -0,0 +1,24 @@
+# Given two strings s and t, determine if they are isomorphic.
+#
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+#
+# All occurrences of a character must be replaced with another character while preserving the order of characters.
+# No two characters may map to the same character, but a character may map to itself.
+
+# --------------- Runtime 42 ms, beats 69.13%. Memory 14.2MB, beats 83.59% ---------------
+
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        compliance = {}
+
+        if len(set(s)) != len(set(t)):
+            return False
+
+        for ind, char in enumerate(s):
+            if char not in compliance:
+                compliance[char] = t[ind]
+            else:
+                if compliance[char] != t[ind]:
+                    return False
+        return True
