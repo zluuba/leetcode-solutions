8c30f8cd515f6bd7f0a29f67b8078d1832122309
zluuba <zluyba.nikitina@gmail.com>
2023-02-16 15:48:37 +0300

    Feat: add [58] problem solution

---
+++ easy-level/[58]-length-of-last-word.py
@@ -0,0 +1,22 @@
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+#
+# A word is a maximal substring consisting of non-space characters only.
+
+
+# --------------- Runtime 23 ms, beats 97.28%. Memory 13.9MB, beats 73.47% ---------------
+
+
+class Solution2:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        last_word = words[-1]
+        return len(last_word)
+
+
+# Alternative solution. One line
+# --------------- Runtime 25 ms, beats 93.61%. Memory 14MB, beats 31.77% ---------------
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len(s.split()[-1])
