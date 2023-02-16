e3ca881bd95ebb26714aca660f8f3151ec5fea5e
zluuba <zluyba.nikitina@gmail.com>
2023-02-16 15:29:23 +0300

    Feat: add [27] problem solution

---
+++ easy-level/[27]-remove-element.py
@@ -0,0 +1,26 @@
+# Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+# The relative order of the elements may be changed.
+#
+# Since it is impossible to change the length of the array in some languages, you must instead have
+# the result be placed in the first part of the array nums. More formally,
+# if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result.
+# It does not matter what you leave beyond the first k elements.
+#
+# Return k after placing the final result in the first k slots of nums.
+#
+# Do not allocate extra space for another array. You must do this by modifying the input
+# array in-place with O(1) extra memory.
+
+# --------------- Runtime 36 ms, beats 63.16%. Memory 13.9MB, beats 53.63% ---------------
+
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        i = 0
+        while val in nums:
+            if nums[i] == val:
+                nums.pop(i)
+                continue
+            i += 1
+
+        return len(nums)
