b289abca47d14e4ebe970d7fa9abb7217246b807
zluuba <zluyba.nikitina@gmail.com>
2023-02-18 14:54:30 +0300

    Fix: update SQL problem solutions

--- SQL/[1693]-daily-leads-and-partners.sql
+++ SQL/[1693]-daily-leads-and-partners.sql
@@ -1,15 +1,23 @@
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | date_id     | date    |
+-- | make_name   | varchar |
+-- | lead_id     | int     |
+-- | partner_id  | int     |
+-- +-------------+---------+
+-- This table does not have a primary key.
+-- This table contains the date and the name of the product sold and the IDs of the lead and partner it was sold to.
+-- The name consists of only lowercase English letters.
+
 -- Write an SQL query that will, for each date_id and make_name, return the number
 -- of distinct lead_id's and distinct partner_id's.
-
 -- Return the result table in any order.
-
 -- The query result format is in the following example.
-
---------------- Runtime 992 ms, beats 42.45% ---------------
 
 
 SELECT date_id, make_name,
 COUNT(DISTINCT lead_id) AS unique_leads,
 COUNT(DISTINCT partner_id) AS unique_partners
 FROM dailysales
-GROUP BY date_id, make_name
+GROUP BY date_id, make_name;
