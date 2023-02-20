25a7b430df2a4f296cc5846bce4aaf91876b498c
zluuba <zluyba.nikitina@gmail.com>
2023-02-20 09:21:36 +0300

    Feat: add [102] problem solution

---
+++ medium-level/[102]-binary-tree-level-order-traversal.py
@@ -0,0 +1,30 @@
+# Given the root of a binary tree, return the level order traversal of its nodes' values.
+# (i.e., from left to right, level by level).
+
+# --------------- Runtime 36 ms, beats 68.67%. Memory 14.2MB, beats 42.97% ---------------
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        stack, result = [root], []
+
+        while stack:
+            length, level = len(stack), []
+
+            for i in range(length):
+                node = stack.pop(0)
+                if node:
+                    level.append(node.val)
+                    stack.append(node.left)
+                    stack.append(node.right)
+
+            if level:
+                result.append(level)
+
+        return result
