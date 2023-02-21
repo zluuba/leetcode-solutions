41bf5dc79cab9fa3aad434d0109ac9bfa62e4816
zluuba <zluyba.nikitina@gmail.com>
2023-02-21 08:47:31 +0300

    Feat: add [540] problem solution

---
+++ medium-level/[540]-single-element-in-a-sorted-array.py
@@ -0,0 +1,25 @@
+# You are given a sorted array consisting of only integers where every element appears exactly twice,
+# except for one element which appears exactly once.
+#
+# Return the single element that appears only once.
+#
+# Your solution must run in O(log n) time and O(1) space.
+
+# --------------- Runtime 155 ms, beats 99.78%. Memory 23.6MB, beats 99.62% ---------------
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: list[int]) -> int:
+        p1, p2 = 0, len(nums) - 1
+
+        while p1 < p2:
+            mid = (p1 + p2) // 2
+
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] != nums[mid + 1]:
+                p2 = mid
+            else:
+                p1 = mid + 2
+
+        return nums[p1]
