84e4ea92cbafe0053bc394555e0b76aa79ff796a
zluuba <zluyba.nikitina@gmail.com>
2023-04-09 10:40:47 +0300

    feat: add 1857 problem solution

---
+++ hard-level/[1857]-largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,42 @@
+# There is a directed graph of n colored nodes and m edges. The nodes are numbered from 0 to n - 1.
+# You are given a string colors where colors[i] is a lowercase English letter representing the color of the ith node
+# in this graph (0-indexed). You are also given a 2D array edges where edges[j] = [aj, bj] indicates
+# that there is a directed edge from node aj to node bj.
+# A valid path in the graph is a sequence of nodes x1 -> x2 -> x3 -> ... -> xk such that there is a directed edge
+# from xi to xi+1 for every 1 <= i < k. The color value of the path is the number of nodes that are colored the most
+# frequently occurring color along that path.
+# Return the largest color value of any valid path in the given graph, or -1 if the graph contains a cycle.
+
+# Example:
+# Input: colors = "abaca", edges = [[0,1],[0,2],[2,3],[3,4]]
+# Output: 3
+# Explanation: The path 0 -> 2 -> 3 -> 4 contains 3 nodes that are colored "a" (red in the above image).
+
+# --------------- Runtime 2365 ms, beats 75%. Memory 104.8MB, beats 47.2% ---------------
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        n = len(colors)
+        indegree = [0] * n
+
+        adj = defaultdict(list)
+        for src, dst in edges:
+            adj[src].append(dst)
+            indegree[dst] += 1
+
+        queue = [i for i in range(n) if indegree[i] == 0]
+        count = [[0] * 26 for _ in range(n)]
+        for i in queue:
+            count[i][ord(colors[i]) - 97] += 1
+            for j in adj[i]:
+                count[j] = list(map(max, count[j], count[i]))
+                indegree[j] -= 1
+                if indegree[j] == 0:
+                    queue.append(j)
+
+        if len(queue) < n:
+            return -1
+        return max(max(i) for i in count)
