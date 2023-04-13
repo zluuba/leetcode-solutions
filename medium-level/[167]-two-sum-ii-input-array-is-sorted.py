5628d3349331c640a461b2f1a514dff7ad54e276
zluuba <zluyba.nikitina@gmail.com>
2023-04-13 09:26:22 +0300

    feat: add medium-level problem solutions

---
+++ medium-level/[167]-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,36 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+# find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1]
+# and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+# Return the indices of the two numbers, index1 and index2,
+# added by one as an integer array [index1, index2] of length 2.
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+# Your solution must use only constant extra space.
+
+# Example 1:
+# Input: numbers = [2,7,11,15], target = 9
+# Output: [1,2]
+# Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+# Example 2:
+# Input: numbers = [2,3,4], target = 6
+# Output: [1,3]
+# Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+# --------------- Runtime 121 ms, beats 93.77%. Memory 14.9MB, beats 80.45% ---------------
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        p1, p2 = 0, len(numbers) - 1
+
+        while p1 < p2:
+            diff = numbers[p1] + numbers[p2]
+            if diff == target:
+                break
+            elif diff > target:
+                p2 -= 1
+            else:
+                p1 += 1
+
+        return [p1 + 1, p2 + 1]
---
+++ medium-level/[946]-validate-stack-sequences.py
@@ -0,0 +1,34 @@
+# Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result
+# of a sequence of push and pop operations on an initially empty stack, or false otherwise.
+
+# Example 1:
+# Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+# Output: true
+# Explanation: We might do the following sequence:
+# push(1), push(2), push(3), push(4),
+# pop() -> 4,
+# push(5),
+# pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+
+# Example 2:
+# Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+# Output: false
+# Explanation: 1 cannot be popped before 2.
+
+# --------------- Runtime 77 ms, beats 36.36%. Memory 14MB, beats 85.31% ---------------
+from typing import List
+
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack = []
+
+        while pushed and popped:
+            if stack and popped[0] == stack[-1]:
+                stack.pop()
+                popped.pop(0)
+            elif pushed:
+                num = pushed.pop(0)
+                stack.append(num)
+
+        return popped == stack[::-1]
