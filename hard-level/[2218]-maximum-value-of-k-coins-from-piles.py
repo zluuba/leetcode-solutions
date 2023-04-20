3f9d53e719a1689f039cb27c4a5d5c1b47e80359
zluuba <zluyba.nikitina@gmail.com>
2023-04-20 11:53:12 +0300

    refactor: add links, examples to hard-level

--- hard-level/[2218]-maximum-value-of-k-coins-from-piles.py
+++ hard-level/[2218]-maximum-value-of-k-coins-from-piles.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/maximum-value-of-k-coins-from-piles/
+
 # There are n piles of coins on a table. Each pile consists of a positive number of coins of assorted denominations.
 # In one move, you can choose any coin on top of any pile, remove it, and add it to your wallet.
 # Given a list piles, where piles[i] is a list of integers denoting the composition of the ith pile from top to bottom,
