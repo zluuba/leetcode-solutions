8c8e7c764921495474505505b0d5f3112321e124
zluuba <zluyba.nikitina@gmail.com>
2023-04-24 12:20:26 +0300

    feat: add 77, 120 problem solutions

---
+++ medium-level/[120]-triangle.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/triangle/description/?envType=study-plan&id=algorithm-i
+
+# Given a triangle array, return the minimum path sum from top to bottom.
+# For each step, you may move to an adjacent number of the row below.
+# More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+# Example 1:
+# Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+# Output: 11
+# Explanation: The triangle looks like:
+#    2
+#   3 4
+#  6 5 7
+# 4 1 8 3
+# The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+
+# Example 2:
+# Input: triangle = [[-10]]
+# Output: -10
+
+# --------------- Runtime 65 ms, beats 78.69%. Memory 14.8MB, beats 99.57% ---------------
+from typing import List
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        length = len(triangle)
+        dp = triangle[-1]
+
+        for i in range(length - 2, -1, -1):
+            for j in range(i + 1):
+                dp[j] = min(dp[j], dp[j + 1]) + triangle[i][j]
+
+        return dp[0]
---
+++ medium-level/[77]-combinations.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/combinations/description/?envType=study-plan&id=algorithm-i
+
+# Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+# You may return the answer in any order.
+
+# Example 1:
+# Input: n = 4, k = 2
+# Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+# Explanation: There are 4 choose 2 = 6 total combinations.
+# Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+
+# Example 2:
+# Input: n = 1, k = 1
+# Output: [[1]]
+# Explanation: There is 1 choose 1 = 1 total combination.
+
+# --------------- Runtime 61 ms, beats 84.66%. Memory 15.2MB, beats 41.36% ---------------
+from typing import List
+from itertools import combinations
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def walker(curr):
+            if len(stack) == k:
+                result.append(stack[:])
+                return
+            if curr > n:
+                return
+
+            stack.append(curr)
+            walker(curr + 1)
+
+            stack.pop()
+            walker(curr + 1)
+
+        stack, result = [], []
+        walker(1)
+        return result
+
+
+# Alternative solution - itertools lib
+# --------------- Runtime 68 ms, beats 99.77%. Memory 14.9MB, beats 98.79% ---------------
+
+class Solution2:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return combinations(range(1, n + 1), k)
