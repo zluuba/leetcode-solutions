8deab7e3c3eb1bed4030fcd2ffe61becddbae833
zluuba <zluyba.nikitina@gmail.com>
2023-11-16 13:00:41 +0300

    feat: add auto-copying of the solution URL to the clipboard in the get_solution_url func

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 from string import ascii_lowercase, digits
+import subprocess
 
 
 BASE = 'https://github.com/zluuba/leetcode/blob/main/'
@@ -11,26 +12,20 @@ def get_filtered_word(word):
     return ''.join(filter(lambda char: char in CHARS, word.lower()))
 
 
-def get_solution_url(level, title):
-    """
-    Makes url for solution in following format:
+def get_solution_url(problem_level, title):
+    """ Makes url for solution in following format:
     https://github.com/zluuba/leetcode/blob/main/[level]/%5B[task_num]%5D-[task_name].py
-
-    Example:
-      input:
-        level: 2
-        title: '1980. Find Unique Binary String'
-
-      output:
-        'https://github.com/zluuba/leetcode/blob/main/medium-level/%5B1980%5D-find-unique-binary-string.py'
     """
 
-    url = BASE + LEVELS[level] + '/'
+    url = BASE + LEVELS[problem_level] + '/'
 
     task_num, task_name = title.split('. ')
     url += f'%5B{task_num}%5D-'
 
-    processed_task_name = filter(lambda word: word, map(lambda word: get_filtered_word(word), task_name.split()))
+    processed_task_name = filter(
+        lambda word: word,
+        map(lambda word: get_filtered_word(word), task_name.split()),
+    )
     url += '-'.join(processed_task_name) + EXTENSION
 
     return url
@@ -38,4 +33,7 @@ def get_solution_url(level, title):
 
 level = 2
 name = ""
-print(get_solution_url(level, name))
+solution_url = get_solution_url(level, name)
+
+# copy solution_url to clipboard
+subprocess.run("pbcopy", text=True, input=solution_url)
